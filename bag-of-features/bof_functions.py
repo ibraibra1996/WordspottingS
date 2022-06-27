4fdee727e0651a61c95b18eabd0ac349f22e8937
Enrico <enrico@familie-romano.de>
2022-06-27 15:47:24 +0200

    renaming

--- bag-of-features/bof_functions.py
+++
@@ -1,9 +0,0 @@
-#pipeline
-#1. Input picture
-#   - build local descriptors
-#   - cluster local descriptors
-#   - find centruids
-#   - build descriptive vector with centruids
-
-# 2. Check distance to other descriptive vector from "Database"
-#   - make descending list of pictures based on distance
