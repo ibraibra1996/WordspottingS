2bc5605490b613aec35fc82b3b196fa5b6581242
yous99tahiri <107937996+yous99tahiri@users.noreply.github.com>
2022-06-27 17:14:21 +0200

    classify2
    classify2

--- common/classification.py
+++ common/classification.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.spatial.distance as ds
 from common.features import BagOfWords, IdentityFeatureTransform
+import scipy.spatial.distance
 
 
 class KNNClassifier(object):
@@ -70,3 +71,49 @@ class KNNClassifier(object):
         distances = ds.cdist(self.__train_samples, test_samples, metric=self.__metric)
         test_labels_indices = np.argmin(distances, axis=0)
         return self.__train_labels[test_labels_indices]
+    from common.features import BagOfWords
+    def classify2(self, test_samples):
+        """Klassifiziert Test Daten.
+        
+        Params:
+            test_samples: ndarray, das Merkmalsvektoren zeilenweise enthaelt (d x t).
+            
+        Returns:
+            test_labels: ndarray, das Klassenlabels spaltenweise enthaelt (d x 1).
+        
+            mit d Testbeispielen und t dimensionalen Merkmalsvektoren.
+        """
+        
+       
+        if self.__train_samples is None or self.__train_labels is None:
+            raise ValueError('Classifier has not been "estimated", yet!')
+        train_samples =self.__train_samples
+        train_labels = self.__train_labels
+        
+        k = self.__k_neighbors
+        metrik = self.__metric
+        #print train_labels
+        #cdist berechnet den abstand zwischen die paare vom array 
+        #jedes array oder zeile wird mit anderen gepaart und abstand gerechnet 
+        distanz = scipy.spatial.distance.cdist(test_samples, train_samples, metrik)
+        # argsort gibt die index in sortierte reihenfolge 
+        #in sortiert wird der erste index mit den kleinsten abstand gespeichert also index vom kleinsten abstand  
+        sortiert = np.argsort(distanz, axis = 1)[:,:k]
+        #copy_train_label als 1 D array alle klassifikationen von trainlabels in copy als 1 d array 
+        copy_train_labels = train_labels.ravel()
+        #jz nehmen wir train labels in den indexen wo wir den 1ten nachbarn fanden 
+        test_labels = copy_train_labels[sortiert]
+       # print(" test_label  :"+str(len(test_labels)) + " SOrtiert :" + str(len(sortiert))) ist gleich 
+        list_test_labels = test_labels.tolist()
+
+        list_copy_train_labels = copy_train_labels.tolist()
+        #print ("list_copy_train_labels")
+        #print (list_copy_train_labels)
+        #print("fertig")
+        Bag = BagOfWords(list_copy_train_labels)
+        listreturn = []
+        for i in list_test_labels:
+            listreturn.append(Bag.most_freq_words(i,1))
+            
+        
+        return np.asarray(listreturn)
