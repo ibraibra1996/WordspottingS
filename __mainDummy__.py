8f35f9cb2a5e1a9167006155a69e28d87f192e90
ibrahim <43646378+ibraibra1996@users.noreply.github.com>
2022-07-10 08:52:49 +0200

    Evaluation

--- __mainDummy__.py
+++ __mainDummy__.py
@@ -11,60 +11,76 @@ from bagOfFeatures.bofFunctions import ImagePrep
 from common import features
 
 
-def main(dataNames,
+def main(dataName,
          step_size,
          cell_size,
          n_centroids,
          step_size_doc,
          cell_size_doc,
          iter):
-    totalAvgAllDocs = 0
+    histograms, namesOfWords = ImagePrep.bof(documentSegmentation=dataName+'.gtp',
+                                             documentImage=dataName+'.png',
+                                             step_size=step_size,
+                                             cell_size=cell_size,
+                                             step_size_doc=step_size_doc,
+                                             cell_size_doc=cell_size_doc,
+                                             n_centroids=n_centroids,
+                                             iter=iter)
 
-    for dataName in dataNames:
-        histograms, namesOfWords = ImagePrep.bof(documentSegmentation=dataName + '.gtp',
-                                                 documentImage=dataName + '.png',
-                                                 step_size=step_size,
-                                                 cell_size=cell_size,
-                                                 step_size_doc=step_size_doc,
-                                                 cell_size_doc=cell_size_doc,
-                                                 n_centroids=n_centroids,
-                                                 iter=iter)
+    # hier kann eine Anfrage gemacht werden
+    # EntriesWith01, Ruckgabeliste = ImagePrep.resultListForPrecision(histograms=histograms,
+    #                                                                namesOfWords=namesOfWords,
+    #                                                               queryIndex=33)
+    # print(EntriesWith01)
+    # print(Ruckgabeliste)
 
-        # hier kann eine Anfrage gemacht werden
-        EntriesWith01, Ruckgabeliste = ImagePrep.resultListForPrecision(histograms=histograms,
-                                                                        namesOfWords=namesOfWords,
-                                                                        queryIndex=33)
-        print(EntriesWith01)
-        print(Ruckgabeliste)
+    precisionForAllWords = ImagePrep.resultListForPrecisionForAllWords(histograms=histograms,
+                                                                       namesOfWords=namesOfWords)
 
-        precisionForAllWords = ImagePrep.resultListForPrecisionForAllWords(histograms=histograms,
-                                                                           namesOfWords=namesOfWords)
+    # print(precisionForAllWords[1])
 
-        # print(precisionForAllWords[1])
+    totalAvg = 0
+    for n in precisionForAllWords:
+        totalAvg += segmentation.Segmentation.averagePrecision(binlist=n)
 
-        totalAvg = 0
-        for n in precisionForAllWords:
-            totalAvg += segmentation.Segmentation.averagePrecision(binlist=n)
+    totalAvg = totalAvg / len(precisionForAllWords)
 
-        totalAvg = totalAvg / len(precisionForAllWords)
+    print(f'totalAvg für {dataName}:{totalAvg}')
 
-        print(f'totalAvg für {dataName}:{totalAvg}')
-        totalAvgAllDocs += totalAvg
-
-    # print(f'totalAvgAllDocs:{totalAvgAllDocs / len(dataNames)}')
     print('_____________')
+    return totalAvg
 
 
 if __name__ == "__main__":
     dataNames = [str(name) + "0" + str(name) for name in range(270, 280)] + [str(name) + "0" + str(name) for name in
                                                                              range(300, 310)]
-    n_centroidsList = [i for i in range(25, 500, 25)]
 
-    main(dataNames=dataNames[10:11],
-         step_size=1,  # 2
-         cell_size=9,
-         step_size_doc=15,
-         cell_size_doc=3,
-         n_centroids=250,  #
+    n_centroidsList = [i for i in range(100, 300, 50)]  # 25, 500, 25
 
-         iter=20)
+    step_sizeList = [i for i in range(2, 10, 2)]  # 1, 10, 1
+    cell_sizeList = [i for i in range(3, 10, 2)]  # 3, 10, 1
+
+    configurationList = [n_centroidsList,
+                         step_sizeList,
+                         cell_sizeList,
+                         dataNames]
+
+    configurationListProduct = []
+    configurationListProduct.append(['n_centroids','step_size','cell_size','totalAvg','dataName'])
+
+    for n_centroids, step_size, cell_size,dataName in itertools.product(*configurationList):
+        totalAvg = main(dataName=dataName,
+                        step_size=step_size,  # 2  1
+                        cell_size=cell_size,  # 3   9
+                        step_size_doc=15,
+                        cell_size_doc=3,
+                        n_centroids=n_centroids,  # 250
+                        iter=20)
+
+        configurationListProduct.append([n_centroids,step_size,cell_size,totalAvg,dataName])
+
+    import numpy as np
+
+    np.savetxt("ResultData.csv", configurationListProduct, delimiter=";", fmt='% s')
+
+    print('csv fertig')
--- __main__.py
+++ __main__.py
@@ -1,85 +1,32 @@
-import os
-import PIL.Image as Image
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 import numpy as np
+import pandas as pd
 
-import bagOfFeatures.segmentation as segmentation
-from bagOfFeatures.bofFunctions import ImagePrep
-
-
+from sklearn import linear_model
+from scipy.stats import spearmanr
 def main():
-    listOfSegmentedImages = list()
-    listOfGTPs = list()
+    df_pie = pd.read_csv('ResultData.csv',
+                         sep=';')
 
-    s1 = segmentation.Segmentation()
+    print('Summary:')
+    print(df_pie[['n_centroids','step_size','cell_size','totalAvg']].describe())
 
-    # _, _, files = next(os.walk("C:\Users\Luca\IdeaProjects\WordspottingS\GT"))
-    gtpfiles = os.listdir(r"\Users\Luca\IdeaProjects\WordspottingS\GT")
-    pngfiles = os.listdir(r"\Users\Luca\IdeaProjects\WordspottingS\pages")
-    nrOfItems = len(gtpfiles)
+    print('\n')
+    print('Correlation:')
+    rho, pval=spearmanr(df_pie['totalAvg'],df_pie[['n_centroids','step_size','cell_size']])
+    print(rho[0])
 
-    # later used for calculation of average
-    totalWordImages = 0
+    print('\n')
+    print('LinearRegression:')
+    X = df_pie[['n_centroids','step_size','cell_size']]
+    y = df_pie['totalAvg']
+    # n_centroids;step_size;cell_size;totalAvg
+    # Initialize model from sklearn and fit it into our data
+    regr = linear_model.LinearRegression()
+    model = regr.fit(X, y)
 
-    ''' segmentiere Bilder 
-    #listOfSegmentedImages beinhaltet geordnet nach Dokument tupel mit je einem segmentierten Wort + repräsentiertem Wort
-    '''
 
-    for i in range(nrOfItems):
-        gtpPath = r"\Users\Luca\IdeaProjects\WordspottingS\GT\\" + gtpfiles[i]
-        pngPath = r"\Users\Luca\IdeaProjects\WordspottingS\pages\\" + pngfiles[i]
-
-        pngImage = Image.open(pngPath, "r")
-        segmentList = s1.segmentCut(pngImage, gtpPath)
-        listOfSegmentedImages.append(segmentList)
-        totalWordImages += len(segmentList)
-
-    listOfAllWords = [inner for outer in listOfSegmentedImages for inner in outer]
-    print(len(listOfAllWords))
-    #print(listOfAllWords)
-    '''
-    hier können BoF Repräsentationen generiert werden
-    '''
-    documentSegmentation = os.path.join('GT', '3010301.gtp')
-    documentImage = os.path.join('pages', '3010301.png')
-    image = Image.open(documentImage)
-
-    wordImageList = segmentation.Segmentation.segmentCut(image, documentSegmentation)
-    print(wordImageList)
-
-    # plt.figure()
-
-    # wordImageList[0][0].show()
-
-    histograms, namesOfWords = ImagePrep.bof(wordImageList=wordImageList)
-    #histograms, namesOfWords = ImagePrep.bof(wordImageList=listOfAllWords)
-
-    precisionForAllWords = []
-
-    for i in range(len(namesOfWords)):
-        precision = ImagePrep.resultListForPrecision(histograms=histograms,
-                                                     namesOfwords=namesOfWords,
-                                                     queryWordHistogram=histograms[i],
-                                                     queryWordString=namesOfWords[i])
-        precisionForAllWords.append(precision)
-
-    '''
-    hier wird jedes Wortbild als Eingabe ausgewählt, um anhand der BoFs ähnliche Worte zu identifizieren
-    '''
-    #av = segmentation.Segmentation.averagePrecision(binlist=precisionForAllWords[4])
-    print(len(precisionForAllWords))
-    print(precisionForAllWords[4])
-    '''
-    Berechnung der Mean Average Precision
-    '''
-    totalAvg = 0
-    for n in precisionForAllWords:
-        totalAvg += segmentation.Segmentation.averagePrecision(binlist=n)
-
-    totalAvg = totalAvg/len(precisionForAllWords)
-
-    print(totalAvg)
+    print('Intercept:', model.intercept_)
+    print('Coefficients:', model.coef_)
 
 if __name__ == "__main__":
     main()
